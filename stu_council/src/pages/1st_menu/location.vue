0691af17ae77a83af51d66db112b0cb1c98062d0
SungMin Kim <sungminkim@SungMinui-MacBookPro.local>
2019-05-09 16:25:40 +0900

    solve high  vulnerability

--- stu_council/src/pages/1st_menu/location.vue
+++ stu_council/src/pages/1st_menu/location.vue
@@ -1,12 +1,18 @@
 <template>
     <div id="location">
         <p>location</p>
+        <DaumMap></DaumMap>
+        <p>위치는 불라불라 입니다.</p>
     </div>
 </template>
 
 <script>
+import DaumMap from '../../components/daum_map'
 export default {
     name: "location",
+    components: {
+        DaumMap
+    },
 }
 </script>
 
--- stu_council/src/pages/home.vue
+++ stu_council/src/pages/home.vue
@@ -1,19 +1,13 @@
 <template>
     <div id="home">
-        <p>위치는 불라불라 입니다.</p>
-        <DaumMap></DaumMap>
         <p>home</p>
         <router-view></router-view>
     </div>
 </template>
 
 <script>
-import DaumMap from '../components/daum_map'
 export default {
     name: 'home',
-    components: {
-        DaumMap
-    },
     methods: {
     },
     
