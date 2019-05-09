f55a0b62024f63e4b80e9127c1c1d585ed3a08a9
spemer <ghsspower@gmail.com>
2019-05-10 02:25:35 +0900

    ADD webpack css-loader

--- webpack.config.js
+++ webpack.config.js
@@ -17,11 +17,49 @@ module.exports = {
           'vue-style-loader',
           'css-loader'
         ],
-      }, {
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'vue-style-loader',
+          'css-loader',
+          'sass-loader'
+        ],
+      },
+      {
+        test: /\.sass$/,
+        use: [
+          'vue-style-loader',
+          'css-loader',
+          'sass-loader?indentedSyntax'
+        ],
+      },
+      {
         test: /\.vue$/,
         loader: 'vue-loader',
         options: {
-          loaders: {}
+          loaders: {
+            // Since sass-loader (weirdly) has SCSS as its default parse mode, we map
+            // the "scss" and "sass" values for the lang attribute to the right configs here.
+            // other preprocessors should work out of the box, no loader config like this necessary.
+            'scss': [
+              'vue-style-loader',
+              'css-loader',
+              {
+                loader: 'sass-loader',
+                options: {
+                  data: `
+                    @import "./src/style/style.scss";
+                  `
+                }
+              }
+            ],
+            'sass': [
+              'vue-style-loader',
+              'css-loader',
+              'sass-loader?indentedSyntax'
+            ]
+          }
           // other vue-loader options go here
         }
       },
