bc7bd16818fc82792bdc833a4cfa787915b15d73
kwang_hoon <kwanghoon222@gmail.com>
2019-05-21 19:39:36 +0900

    update sign_up

--- src/pages/member/login.vue
+++ src/pages/member/login.vue
@@ -41,11 +41,11 @@ export default {
           axios.post('http://117.16.231.66:7003/login/signIn',
           {id: self.id, passwd: self.password})
           .then(function(response) {
-            self.store_token = response.data.token
-            self.$session.start()
-            self.$session.set('member_token', self.store_token)
-            window.location.href ='/'  // 로그인 전으로 이동하면 더 좋을 것 같음, self.$router.go(-1)로 할 경우 로그아웃 후 바로 로그인 했을시 바로 로그아웃 되버리는 현상 존재
-            console.log(response)
+              self.store_token = response.data.token
+              self.$session.start()
+              self.$session.set('member_token', self.store_token)
+              window.location.href ='/'  // 로그인 전으로 이동하면 더 좋을 것 같음, self.$router.go(-1)로 할 경우 로그아웃 후 바로 로그인 했을시 바로 로그아웃 되버리는 현상 존재
+              console.log(response)
             })
             .catch(function(error) {
               if(error.response.data.ans == self.password_error) {
@@ -65,7 +65,7 @@ export default {
       check_session() {
         if(this.$session.exists()) {
           alert("로그인 상태로 접근할 수 없는 페이지입니다.")
-          this.$router.push({name:'home'}) // 위와 마찬가지
+          this.$router.push({name:'home'}) // 위와 마찬가지, signup.vue에도 똑같은 함수 존재
         }
       }
   },
--- src/pages/member/signup.vue
+++ src/pages/member/signup.vue
@@ -1,10 +1,112 @@
 <template>
-    
+    <div id="signup">
+            <h1>회원가입</h1>
+            학번 <input type="text" v-model="sign_id"><br>
+            비밀번호 <input type="password" v-model="sign_password"><br>
+            비밀번호 확인<input type="password" v-model="sign_check_password"><br>
+            이름 <input type="text" v-model="sign_name"><br>
+            핸드폰 <input type="text" v-model="sign_phone"><br>
+            학과 <select v-model="sign_major">
+                <option value="정보통신공학과">정보통신공학과</option>
+                <option value="">-----</option>
+            </select><br>
+            <input type="checkbox" v-model="sign_agree" value="agree">
+            <label>이용약관, 개인정보취급방침에 동의합니다.</label>
+            <input @click="bt_listener" type="submit" value="회원가입">
+    </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
-    name: 'signup'
+    name: 'signup',
+
+    created() {
+        this.check_session()
+    },
+
+    data: function() {
+        return {
+            sign_id: '',
+            sign_password: '',
+            sign_check_password: '',
+            sign_name: '',
+            sign_phone: '',
+            sign_major: '',
+            sign_agree: '',
+            check_sign_success: 'success',
+            check_sign_fail: 'Id',
+        }
+    },
+
+    methods: {
+        check_session() {
+            if(this.$session.exists()) {
+            alert("로그인 상태로 접근할 수 없는 페이지입니다.")
+            this.$router.push({name:'home'}) // 고쳤으면함
+            }
+        },
+
+        bt_listener() {
+            var self = this
+            if(self.sign_id == '') {
+                alert("학번을 입력해주세요.")
+            }
+            else if(self.sign_password == '') {
+                alert("비밀번호를 입력해주세요.")
+            }
+            else if(self.sign_name == '') {
+                alert("이름을 입력해주세요.")
+            }
+            else if(self.sign_phone == '') {
+                alert("휴대폰 번호를 입력해주세요.")
+            }
+            else if(self.sign_major == '') {
+                alert("학과를 선택해주세요.")
+            }
+            else if(!(self.sign_password == self.sign_check_password)) {
+                alert("두 비밀번호가 동일해야합니다.")
+            }
+            else if(self.sign_password.length < 6 || self.sign_password.length >14) {
+                alert("비밀번호는 6~14자로 입력해주세요.")
+            }
+            else if(self.sign_agree == '') {
+                alert("이용약관, 개인정보취급방침 동의에 체크해주세요.")
+            }
+            else {
+                self.go_signup(self.sign_id, self.sign_password, self.sign_name, self.sign_phone, self.sign_major)
+            }
+        },
+
+        go_signup(sign_id, sign_password, sign_name, sign_phone, sign_major) {
+            var self = this
+            axios.post('http://117.16.231.66:7003/login/signUp',
+            {
+                id: self.sign_id,
+                passwd: self.sign_password,
+                tel: self.sign_phone,
+                major: self.sign_major,
+                name: self.sign_name
+            })
+            .then(function(response) {
+                if(response.data.ans == self.check_sign_success) {
+                    self.$router.push({name:'signupsuccess'})
+                    console.log(response)
+                }
+            })
+            .catch(function(error) {
+                if(error.response.data.ans == self.check_sign_fail) {
+                    alert("이미 회원가입된 학번입니다.")
+                }
+                else {
+                    alert("error")
+                }
+                console.log(error.response)
+            })            
+        }
+
+    },
+
 }
 </script>
 
