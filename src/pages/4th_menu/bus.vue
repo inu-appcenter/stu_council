e0f48ea4ec32b803fd721aef83c618097b511329
spemer <ghsspower@gmail.com>
2019-05-27 18:06:31 +0900

    DEL duplicated style

--- src/pages/4th_menu/bus.vue
+++ src/pages/4th_menu/bus.vue
@@ -31,67 +31,6 @@
   </div>
 </template>
 
-<style>
-/* table {
-  width: 100%;
-  border-spacing: 0px;
-}
-
-tr,
-td {
-  text-align: center;
-  padding: 10px 10px;
-  border-bottom: 1px solid #111111;
-}
-
-#title.headTd {
-  padding: 20px 0px;
-}
-
-.contentTd {
-  font-size: 15px;
-  text-align: left;
-  color: #111111;
-}
-
-.headTd {
-  font-size: 15px;
-  color: #111111;
-}
-
-#bus.container {
-  padding: 90px 128px;
-}
-
-.body_container {
-  display: flex;
-  width: 100%;
-}
-
-#notice_contents {
-  margin-left: 7%;
-  width: 100%;
-}
-
-#content_name {
-  display: flex;
-  height: 34px;
-  width: 100%;
-  font-size: 23px;
-  font-weight: bold;
-  color: #003e8f;
-}
-
-#content_body {
-  margin-top: 7px;
-  width: 100%;
-  background-color: #ffffff;
-  border-top: 3px solid #000000;
-  border-bottom: 3px solid #000000;
-} */
-</style>
-
-
 <script>
 import customNavigation from '@/pages/4th_menu/custom_navigation'
 import axios from 'axios'
--- src/pages/4th_menu/rental.vue
+++ src/pages/4th_menu/rental.vue
@@ -49,64 +49,6 @@
   text-align: center;
   color: #ffffff;
 }
-/* table {
-  width: 100%;
-  border-spacing: 0px;
-}
-
-tr,
-td {
-  text-align: center;
-  padding: 10px 10px;
-  border-bottom: 1px solid #111111;
-}
-
-#title.headTd {
-  padding: 20px 0px;
-}
-
-.contentTd {
-  font-size: 15px;
-  text-align: left;
-  color: #111111;
-}
-
-.headTd {
-  font-size: 15px;
-  font-weight: bold;
-  color: #111111;
-}
-
-#rental.container {
-  padding: 90px 128px;
-}
-
-.body_container {
-  display: flex;
-  width: 100%;
-}
-
-#notice_contents {
-  margin-left: 7%;
-  width: 100%;
-}
-
-#content_name {
-  display: flex;
-  height: 34px;
-  width: 100%;
-  font-size: 23px;
-  font-weight: bold;
-  color: #003e8f;
-}
-
-#content_body {
-  width: 100%;
-  margin-top: 7px;
-  background-color: #ffffff;
-  border-top: 3px solid #000000;
-  border-bottom: 3px solid #000000;
-} */
 </style>
 
 
