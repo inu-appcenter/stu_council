126444ce781fd22208509c45e48d6d234b0b9ccc
DESKTOP-TA2I9Q2\limso <limson_95@naver.com>
2019-09-15 16:19:26 +0900

    ..

--- build/src/store/getters.js
+++
@@ -1,9 +0,0 @@
-export default {
-  // get_user_value: state => {
-  //   let val = [];
-  //   [...state.user_info].forEach(user => {
-  //     val.concat(user.value)
-  //   })
-  //   return val
-  // }
-}
