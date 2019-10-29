175d3181e9df9f155c79e605417080d53b8bbeb8
kwang_hoon <kwanghoon222@gmail.com>
2019-10-29 23:47:14 +0900

    add vote img

--- src/main.js
+++ src/main.js
@@ -1,32 +1,31 @@
-import 'babel-polyfill'
-import 'es6-promise/auto'
-import 'event-source-polyfill'
-import 'promise-polyfill/src/polyfill'
+import "babel-polyfill"
+import "es6-promise/auto"
+import "event-source-polyfill"
+import "promise-polyfill/src/polyfill"
 
-import Vue from 'vue'
-import App from './App'
-import store from '@/store'
-import router from '@/router'
-import VueLodash from 'vue-lodash'
-import axios from 'axios'
-import {
-  polyfill
-} from 'es6-promise'
+import Vue from "vue"
+import App from "./App"
+import store from "@/store"
+import router from "@/router"
+import VueLodash from "vue-lodash"
+import axios from "axios"
+import { polyfill } from "es6-promise"
 
 Vue.config.productionTip = true
 Vue.prototype.$http = axios
 // .env 에서 카카오 키 관리
 //Kakao.init(process.env.KAKAO_KEY)
 
-
 //vue-session
-import VueSession from 'vue-session'
+import VueSession from "vue-session"
 Vue.use(VueSession)
 Vue.use(VueLodash)
 
+import VueA11yDialog from "vue-a11y-dialog"
+Vue.use(VueA11yDialog)
 
 new Vue({
-  el: '#app',
+  el: "#app",
   router,
   store,
   render: h => h(App)
--- src/pages/home.vue
+++ src/pages/home.vue
@@ -2,9 +2,13 @@
   <div id="home">
     <div class="home-top-section">
       <div class="home-top-img">
-        <img class="home-main-pic" src="../assets/main.png">
+        <img class="home-main-pic" src="../assets/main.png" />
+        <img
+          class="temp_img"
+          src="../assets/temp_img.png"
+          onclick="window.open('http://vote.inu.ac.kr/')"
+        />
       </div>
-      <div class="home-top-text"></div>
     </div>
 
     <div class="home-middle-section">
@@ -14,7 +18,7 @@
             <span class="wt-1">업무시간</span>
             <span class="wt-2">오전 10시 ~ 오후 5시</span>
           </div>
-          <div class="line-block"/>
+          <div class="line-block" />
           <div class="A-E-C">
             <p>Adress</p>
             <p>Email</p>
@@ -37,14 +41,14 @@
     <div class="home-bottom-section">
       <div class="bottom-container">
         <div class="facebook-block" onclick="window.open('https://www.facebook.com/inuchong/')"></div>
-        <div class="padding-block"/>
+        <div class="padding-block" />
 
         <div class="Every-Kakao-block">
           <div class="Every-block" onclick="window.open('https://inu.everytime.kr/416066')"></div>
           <div class="Kakao-block" onclick="window.open('http://pf.kakao.com/_AStxoxl')"></div>
         </div>
 
-        <div class="padding-block"/>
+        <div class="padding-block" />
 
         <div class="Insta-Ad-block">
           <div class="Insta-block" onclick="window.open('https://www.instagram.com/36inu_bounce/')"></div>
@@ -56,7 +60,13 @@
     <router-view></router-view>
   </div>
 </template>
-
+<style>
+.temp_img {
+  width: 467px;
+  height: 80px;
+  cursor: pointer;
+}
+</style>
 <script>
 import bfLogin from "@/components/home_bf_login";
 import afLogin from "@/components/home_af_login";
